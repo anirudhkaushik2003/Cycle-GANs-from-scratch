9f1ebe141a090458021a4133ceed350451056648
Anirudh Kaushik <>
2023-10-14 14:53:53 +0530

    removed sigmoid, conv5 from disc 30x30 size

--- discriminator.py
+++ discriminator.py
@@ -6,9 +6,9 @@ import torch.nn.functional as F
 class Block(nn.Module):
     def __init__(self, in_ch, out_ch, stride=2, apply_norm=True):
         super(Block, self).__init__()
-        self.conv = nn.Conv2d(in_ch, out_ch, 4, stride, padding=1, padding_mode="replicate")
+        self.conv = nn.Conv2d(in_ch, out_ch, 4, stride, padding=1)
         self.norm = nn.InstanceNorm2d(out_ch)
-        self.relu = nn.LeakyReLU(0.2)
+        self.relu = nn.LeakyReLU(0.2, True)
         self.apply_norm = apply_norm
 
     def forward(self, x):
@@ -30,16 +30,14 @@ class Discriminator(nn.Module):
         self.conv2 = Block(64, 128) # 64
         self.conv3 = Block(128, 256) # 32
 
-        # self.conv4 = Block(256, 512) # 16
-        self.conv5 = Block(256, 512, stride=1) # 15
-        self.out = nn.Conv2d(512, 1, 4, padding=1) # 15
+        self.conv4 = Block(256, 512, stride=1) # 30
+        self.out = nn.Conv2d(512, 1, 4, padding=1) # 30
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        # x = self.conv4(x)
-        x = self.conv5(x)
+        x = self.conv4(x)
         x = self.out(x)
 
         return x
